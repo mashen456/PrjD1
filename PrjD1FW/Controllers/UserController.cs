13f53fdb343a5dfecaa7d09a4201dc95b30fe91c
hp-laptop <fischer456@gmail.com>
2020-12-15 14:13:32 +0100

    UserController added

---
+++ PrjD1FW/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrjD1FW.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public string Index()
+        {
+            return "working";
+        }
+    }
+}
